0c47904aefbc834dcd9ea051329b2b1bf4a9df81
Renato Torres <renato.torres@outsystems.com>
2021-01-01 19:26:00 +0000

    fix: installation of microk8s
    simplification by removing docker, apt dist-upgrade, etc.

--- tools/generate_k8s_token.py
+++ tools/generate_k8s_token.py
@@ -1,15 +1,20 @@
 
-import string
 import random
+import string
 
 # [a-z0-9]{6}.[a-z0-9]{16}"
 
-def generate_token():
-    letters = string.ascii_lowercase + string.digits
-    part1 = ''.join(random.choice(letters) for i in range(6))
-    part2 = ''.join(random.choice(letters) for i in range(16))
 
-    print(f"{part1}.{part2}")
+def generate_token(microk8s_mode=True):
+    letters = string.ascii_lowercase + string.digits
+
+    if microk8s_mode:
+        print(''.join(random.choice(letters) for i in range(36)))
+    else:
+        part1 = ''.join(random.choice(letters) for i in range(6))
+        part2 = ''.join(random.choice(letters) for i in range(16))
+        print(f"{part1}.{part2}")
+
 
 if __name__ == "__main__":
     generate_token()
